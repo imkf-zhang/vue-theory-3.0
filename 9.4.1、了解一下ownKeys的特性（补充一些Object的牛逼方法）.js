d5d7533003ee2be64708d39349f972244a58f007
zhangkaifan <15638452918@163.com>
2024-04-20 09:41:38 +0800

    feat: hasOwnProperty了解

--- 9.4.1、了解一下ownKeys的特性（补充一些Object的牛逼方法）.js
+++ 9.4.1、了解一下ownKeys的特性（补充一些Object的牛逼方法）.js
@@ -15,3 +15,11 @@ console.log(Reflect.ownKeys(obj)) // [ 'a', 'b', Symbol(baz) ]
 console.log(Object.getOwnPropertyNames(obj))  // [ 'a', 'b' ]
 
 console.log(Object.getOwnPropertySymbols(obj)) // [ Symbol(baz) ]
+
+/**
+ * Object.prototype.hasOwnProperty()  
+ * 返回一个布尔值，表示对象自有属性（而不是继承来的属性）中是否具有指定的属性。
+ */
+let obj1 = {a: 'a'}
+console.log(obj1.hasOwnProperty('a'))  // true
+console.log(obj1.hasOwnProperty('toString')) // false
